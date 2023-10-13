2cc46248aa9f6adca12b68503c4e09c746e070d9
Ivan Nikitin <ivannikitin9853@gmail.com>
2023-10-13 22:19:58 +0700

    fix file video

--- src/video.py
+++ src/video.py
@@ -2,7 +2,6 @@ from googleapiclient.discovery import build
 
 import os
 
-
 API_KEY = os.getenv('YT_API_KEY')
 service = build('youtube', 'v3', developerKey=API_KEY)
 
@@ -19,14 +18,13 @@ class Video:
         self.id_video = id_video
         self.url_video = f'https://www.youtube.com/watch?v={id_video}'
         response = service.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=id_video).execute()
+                                         id=id_video).execute()
         self.video_title = response['items'][0]['snippet']['title']
         self.count_view = response['items'][0]['statistics']['viewCount']
         self.like_counts = response['items'][0]['statistics']['likeCount']
 
-
     def __str__(self):
-        return f'{self.video_title} - {self.url_video}'
+        return f'{self.video_title}'
 
 
 class PLVideo(Video):
@@ -40,5 +38,3 @@ class PLVideo(Video):
         """
         super().__init__(id_video)
         self.id_playlist = id_playlist
-
-
