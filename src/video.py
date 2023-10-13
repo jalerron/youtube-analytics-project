31ee8518ef6f6a78bb796738de3623e506a65543
Ivan Nikitin <ivannikitin9853@gmail.com>
2023-10-13 13:04:21 +0700

    add new file with classes Video and PLVideo

---
+++ src/video.py
@@ -0,0 +1,44 @@
+from googleapiclient.discovery import build
+
+import os
+
+
+API_KEY = os.getenv('YT_API_KEY')
+service = build('youtube', 'v3', developerKey=API_KEY)
+
+
+class Video:
+    """
+    Класс для видео
+    """
+
+    def __init__(self, id_video: str):
+        """
+        Инициализация класса Видео
+        """
+        self.id_video = id_video
+        self.url_video = f'https://www.youtube.com/watch?v={id_video}'
+        response = service.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=id_video).execute()
+        self.video_title = response['items'][0]['snippet']['title']
+        self.count_view = response['items'][0]['statistics']['viewCount']
+        self.like_counts = response['items'][0]['statistics']['likeCount']
+
+
+    def __str__(self):
+        return f'{self.video_title} - {self.url_video}'
+
+
+class PLVideo(Video):
+    """
+    Класс для плейлиста
+    """
+
+    def __init__(self, id_video, id_playlist):
+        """
+        Инициализация для класса плейлиста
+        """
+        super().__init__(id_video)
+        self.id_playlist = id_playlist
+
+
